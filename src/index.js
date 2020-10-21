5f95911c0c4e6909abf9cb18414b4f8e15435092
Rozum Alena <rozum.elena@gmail.com>
2020-10-21 16:04:59 +0300

    init: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const stringNum = Math.abs(n).toString().split('').reverse().join('');
+    let result = (stringNum.startsWith('0')) ? stringNum.substring(1) : stringNum;
+ return result;
 }
